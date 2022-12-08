4d9c64c9aa1cfc0db673d4e6a5b6c7db5e57f238
Hiroji <joegilbertdev@outlook.com>
2022-12-08 20:51:01 +0000

    About- adjusted css to remove span from About.vue

--- vue-project/src/components/About.vue
+++ vue-project/src/components/About.vue
@@ -5,11 +5,10 @@
 <template>
     <section class="header__intro__section" id="about">
         <div class="about__grid__container">
-            <h1 class="header__intro__h1">Hiya, I'm Joe Gilbert <br> and I'm a full-stack Developer.<span>
-                    <div class="header__intro__tech">
-                        <p>place stack here</p>
-                    </div>
-                </span></h1>
+            <h1 class="header__intro__h1">Hiya, I'm Joe Gilbert <br> and I'm a full-stack Developer.</h1>
+            <div class="header__intro__tech">
+                <p>place stack here</p>
+            </div>
             <img class="header__intro__img" src="../assets/img_1.jpg"
                 alt="an image of myself on holiday in Murano, Venice, Italy">
 
