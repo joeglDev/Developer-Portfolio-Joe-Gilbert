d0221f344a77c78c0a07a993d72cb221092fdbd2
Hiroji <joegilbertdev@outlook.com>
2022-12-09 21:47:43 +0000

    NavBar- removed section from nav

--- vue-project/src/components/NavBar.vue
+++ vue-project/src/components/NavBar.vue
@@ -8,7 +8,6 @@
             <li><a href="#about">About</a></li>
             <li><a href="#tech">Technologies</a></li>
             <li><a href="#projects">My projects</a></li>
-            <li><a href="#experience">Experience and Education</a></li>
             <li><a href="#contact">Contact</a></li>
             <li><a target="_blank" href="https://www.dropbox.com/s/x2in4iz3di30gej/2022-11-CV-JG-V12.pdf?dl=0"
                     download="file">Download my CV</a></li>
