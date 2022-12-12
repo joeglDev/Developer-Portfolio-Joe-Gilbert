1c2abeca91fa8b52d34d33e6bb1f9b91dc7414a1
Hiroji <joegilbertdev@outlook.com>
2022-12-12 22:56:25 +0000

    About- fixed grid template area CSS

--- vue-project/src/components/About.vue
+++ vue-project/src/components/About.vue
@@ -11,11 +11,11 @@ const changeHeader = (event: Event) => {
 <template>
     <section class="header__intro__section" id="about">
         <div class="about__grid__container">
-            <h1 class="header__intro__h1" @click="changeHeader">Hiya, I'm Joe Gilbert <br> and I'm a full-stack
+            <h1 class="header__intro__h1 heading" @click="changeHeader">Hiya, I'm Joe Gilbert <br> and I'm a full-stack
                 Developer.</h1>
-            <img class="header__intro__img" src="../assets/img_1.jpg"
+            <img class="header__intro__img img" src="../assets/img_1.jpg"
                 alt="an image of myself on holiday in Murano, Venice, Italy">
-            <div class="header__intro__tech">
+            <div class="header__intro__tech tech">
                 <p>Having worked in academic science and medical diagnostics I thought it was time for a change and so I
                     used my previous experience to dive into the world of web development ^w^</p>
                 <p>Click the <span class="header__intro__tech__span">above title for a fun fact</span> about myself or
