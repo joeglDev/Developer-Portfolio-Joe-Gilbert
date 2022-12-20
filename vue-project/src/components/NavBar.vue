3d3f4e9c89bebda09039656f09d1cd7a10219fa6
Hiroji <joegilbertdev@outlook.com>
2022-12-20 20:29:06 +0000

    2-navbar- functioning mobile navbar

--- vue-project/src/components/NavBar.vue
+++ vue-project/src/components/NavBar.vue
@@ -3,32 +3,14 @@
 </script>
 
 <template>
-    <!--
-    <nav>
-        <ul>
-            <li><a href="#about">About</a></li>
-            <li><a href="#tech">Technologies</a></li>
-            <li><a href="#projects">My projects</a></li>
-            <li><a href="#contact">Contact</a></li>
-            <li><a target="_blank" href="https://www.dropbox.com/s/x2in4iz3di30gej/2022-11-CV-JG-V12.pdf?dl=0"
-                    download="file">Download my CV</a></li>
-        </ul>
-    </nav>
--->
+  
     <nav class="navbar">
-
-
-
         <!-- NAVIGATION MENU -->
-
-        <ul class="nav-links">
-
+        <ul class="navbar__ul">
             <!-- USING CHECKBOX HACK -->
-            <div class="navbar__icon__div">
-                <input type="checkbox" id="checkbox_toggle" />
+            <input type="checkbox" id="checkbox_toggle" />
+            <label for="checkbox_toggle" class="hamburger">&#9776;</label>
 
-                <label for="checkbox_toggle" class="hamburger">&#9776;</label>
-            </div>
             <!-- NAVIGATION MENUS -->
 
             <div class="menu">
