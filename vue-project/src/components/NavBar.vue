b0e97467558e453aad00b22dca1ba5caabaa0727
Hiroji <joegilbertdev@outlook.com>
2022-12-20 20:34:47 +0000

    2-navbar- consistified class names

--- vue-project/src/components/NavBar.vue
+++ vue-project/src/components/NavBar.vue
@@ -8,12 +8,12 @@
         <!-- NAVIGATION MENU -->
         <ul class="navbar__ul">
             <!-- USING CHECKBOX HACK -->
-            <input type="checkbox" id="checkbox_toggle" />
-            <label for="checkbox_toggle" class="hamburger">&#9776;</label>
+            <input type="checkbox" id="toggle__navbar" />
+            <label aria-label="click to toggle display of nav bar" for="toggle__navbar" class="navbar__ul__hamburger">&#9776;</label>
 
             <!-- NAVIGATION MENUS -->
 
-            <div class="menu">
+            <div class="navbar__ul__lists">
 
                 <li><a class="navbar__a" href="#about">About</a></li>
                 <li><a class="navbar__a" href="#tech">Technologies</a></li>
@@ -22,12 +22,8 @@
                 <li><a class="navbar__a" target="_blank"
                         href="https://www.dropbox.com/s/x2in4iz3di30gej/2022-11-CV-JG-V12.pdf?dl=0"
                         download="file">Download my CV</a></li>
-
-
             </div>
-
         </ul>
-
     </nav>
 
 </template>
