566651ee96f7bc3249b52a7b5c336e78a7cfffe8
Hiroji <joegilbertdev@outlook.com>
2023-01-10 11:56:40 +0000

    About- removed legacy js

--- vue-project/src/components/About.vue
+++ vue-project/src/components/About.vue
@@ -1,18 +1,23 @@
 <script setup lang="ts">
+import {ref} from 'vue';
+
+//states
+const headingContent = ref(`Hi, I'm Joe Gilbert 
+and I'm a full-stack Developer.`);
 const funFactsArr = ["My current hair colour is pink.", "I really love the colours of Trans Pennine Express trains (2022).", "The vast majority of this website was coded whilst listining to the Japanese pop band 'Perfume'.", "I currently have 5 different LGBTQ+ pride flags hung up in my house.", "I'm planning a motorcycling holiday around the north coast of Scotland."];
 
 const changeHeader = (event: Event) => {
-    const heading = document.getElementsByClassName("header__intro__h1")[0];
     const randomNumber = Math.floor(Math.random() * (funFactsArr.length));
-    heading.innerHTML = funFactsArr[randomNumber];
+    const newHeadingContent = funFactsArr[randomNumber];
+    headingContent.value = newHeadingContent;
+
 };
 </script>
 
 <template>
     <section class="header__intro__section" id="about">
         <div class="about__grid__container">
-            <h1 class="header__intro__h1 heading" @click="changeHeader">Hiya, I'm Joe Gilbert <br> and I'm a full-stack
-                Developer.</h1>
+            <h1 class="header__intro__h1 heading" @click="changeHeader">{{headingContent}}</h1>
             <img class="header__intro__img img" src="../assets/img_1.jpg"
                 alt="an image of myself on holiday in Murano, Venice, Italy">
             <div class="header__intro__tech tech">
