3539dfd237a0d389e01d4a88d6de7dd9ac5b6be8
Hiroji <joegilbertdev@outlook.com>
2023-01-12 21:45:19 +0000

    About: dynaically change image on img click

--- vue-project/src/components/About.vue
+++ vue-project/src/components/About.vue
@@ -1,25 +1,40 @@
 <script setup lang="ts">
-import {ref} from 'vue';
+import { ref } from 'vue';
+import type { imageObject } from '../../Interfaces';
+
+//data
+const funFactsArr = ["My current hair colour is pink.", "I really love the colours of Trans Pennine Express trains (2022).", "The vast majority of this website was coded whilst listining to the Japanese pop band 'Perfume'.", "I currently have 5 different LGBTQ+ pride flags hung up in my house.", "I'm planning a motorcycling holiday around the north coast of Scotland."];
+const imageArr: imageObject[] = [{ name: "img_1.jpg", alt: "an image of myself on holiday in Murano, Venice, Italy" }, { name: "img_2.jpg", alt: "an image of myself against the lgbtq progress flag. Long straight hair, bleached pink" }];
 
 //states
 const headingContent = ref(`Hi, I'm Joe Gilbert 
 and I'm a full-stack Developer.`);
-const funFactsArr = ["My current hair colour is pink.", "I really love the colours of Trans Pennine Express trains (2022).", "The vast majority of this website was coded whilst listining to the Japanese pop band 'Perfume'.", "I currently have 5 different LGBTQ+ pride flags hung up in my house.", "I'm planning a motorcycling holiday around the north coast of Scotland."];
 
+const currentProfileImage = ref(imageArr[0]);
+
+//methods
 const changeHeader = (event: Event) => {
     const randomNumber = Math.floor(Math.random() * (funFactsArr.length));
     const newHeadingContent = funFactsArr[randomNumber];
     headingContent.value = newHeadingContent;
-
 };
+
+const changeProfileImage = (event: Event) => {
+    const newProfileImgage = imageArr[Math.floor(Math.random() * (imageArr.length))];
+    currentProfileImage.value = newProfileImgage;
+}
+
+const getImgUrl = (name: string) => {
+    return new URL(`../assets/${name}`, import.meta.url).href
+}
 </script>
 
 <template>
     <section class="header__intro__section" id="about">
         <div class="about__grid__container">
-            <h1 class="header__intro__h1 heading" @click="changeHeader">{{headingContent}}</h1>
-            <img class="header__intro__img img" src="../assets/img_1.jpg"
-                alt="an image of myself on holiday in Murano, Venice, Italy">
+            <h1 class="header__intro__h1 heading" @click="changeHeader">{{ headingContent }}</h1>
+            <img class="header__intro__img img" :src="getImgUrl(currentProfileImage.name)"
+                v-bind:alt="`${currentProfileImage.alt}`" @click="changeProfileImage">
             <div class="header__intro__tech tech">
                 <p>Having worked in academic science and medical diagnostics I thought it was time for a change and so I
                     used my previous experience to dive into the world of web development ^w^</p>
